57b8af58f2507ba1e85066fcdcdf266b2be99953
Shakur Smith <smith.shakur@ymail.com>
2021-04-21 16:37:16 -0500

    added validation middleware

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
 import "./App.css";
 import ErrorAlert from "./ErrorAlert";
 import InputForm from "./InputForm";
--- server/distance/distance.controller.js
+++ server/distance/distance.controller.js
@@ -1,14 +1,42 @@
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
+function hasText(req, res, next) {
+  const { data: { first_string, second_string } = {} } = req.body;
+
+  if (first_string && second_string) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: "Length of 'string' property must be greater than 0.",
+  });
+}
+
+function hasValidText(req, res, next) {
+  const { data: { first_string, second_string } = {} } = req.body;
+  let validator = /^[a-zA-Z]+$/;
+
+  if (!validator.test(first_string) || !validator.test(second_string)) {
+    next({
+      status: 400,
+      message:
+        "A 'string' property may only include letters A-Z. No spaces or special characters allowed.",
+    });
+  }
+
+  return next();
+}
+
 async function create(req, res, next) {
-  // const newRestaurant = ({ restaurant_name, cuisine, address } = req.body.data);
-  console.log("Ready to create something!");
-  return res.status(201).json({ data: "Ready to create something!" });
-  // restaurantsService
-  //   .create(newRestaurant)
-  //   .then((newRestaurant) => res.status(201).json({ data: newRestaurant[0] }));
+  const newData = ({ first_string, second_string } = req.body.data);
+  console.log(newData);
+  return await res.status(201).json({ data: newData });
 }
 
 module.exports = {
-  create: [asyncErrorBoundary(create)],
+  create: [
+    asyncErrorBoundary(hasText),
+    asyncErrorBoundary(hasValidText),
+    asyncErrorBoundary(create),
+  ],
 };
